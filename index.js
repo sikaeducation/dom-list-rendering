60a7009a432fba03c81949ff66832a016d4a22d3
Kyle Coberly <kyle.coberly@gmail.com>
2022-03-29 12:29:20 -0600

    Add data

---
+++ index.js
@@ -0,0 +1,17 @@
+const title = "Sika Education"
+const units = [{
+  id: 1,
+  name: "Websites"
+}, {
+  id: 2,
+  name: "Web Apps"
+},{
+  id: 3,
+  name: "Problem Solving"
+},{
+  id: 1,
+  name: "Angular"
+},{
+  id: 1,
+  name: "Java"
+}]
